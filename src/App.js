fbceb99383d4f42a38f239bf755e59b9d24aa312
rasedul99 <rasedul2199@gmail.com>
2022-10-01 03:09:32 +0600

    card responsive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 function App() {
   return (
     <div className="h-screen flex  items-center justify-center bg-gray-100">
-      <div className="max-w-sm p-8 bg-white rounded-lg shadow-md space-y-2">
+      <div className="max-w-sm p-8 bg-white rounded-lg shadow-md space-y-2 sm:flex sm:gap-x-5 sm:py-4 sm:space-y-0 sm:items-center">
         <img
-          className="h-24 w-24 mx-auto rounded-full ring-4 ring-green-400"
+          className="h-24 w-24 sm:h-32 sm:w-32 mx-auto sm:mx-0 sm:flex-shrink-0 rounded-full ring-4 ring-green-400"
           src="rasedul.jpg"
           alt="demo"
         />
-        <div className="text-center space-y-2">
+        <div className="text-center sm:text-left  space-y-2">
           <div>
-            <p className="text-lg text-black font-semibold">Learn With Rased</p>
+            <p className="text-lg  text-black font-semibold">
+              Learn With Rased
+            </p>
             <p className="text-gray-500">Youtube Channel</p>
           </div>
-          <button className="px-3 py-1 border border-purple-200 rounded-full text-purple-600 text-sm font-semibold">
+          <button className="px-3 py-1 border  border-purple-200 rounded-full text-purple-600 text-sm font-semibold">
             Visit Now
           </button>
         </div>
