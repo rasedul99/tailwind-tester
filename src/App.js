fbf473870ab7f81bbd05804bed63c8d9b8a0c04e
rasedul99 <rasedul2199@gmail.com>
2022-09-30 15:58:57 +0600

    card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex  items-center justify-center bg-gray-100">
+      <div className="max-w-sm p-8 bg-white rounded-lg shadow-md space-y-2">
+        <img
+          className="h-24 w-24 mx-auto rounded-full ring-4 ring-green-400"
+          src="rasedul.jpg"
+          alt="demo"
+        />
+        <div className="text-center space-y-2">
+          <div>
+            <p className="text-lg text-black font-semibold">Learn With Rased</p>
+            <p className="text-gray-500">Youtube Channel</p>
+          </div>
+          <button className="px-3 py-1 border border-purple-200 rounded-full text-purple-600 text-sm font-semibold">
+            Visit Now
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
